d31986ae8e93023527481ca58f2d63d147b03b91
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2022-05-28 22:18:10 +0300

    Add a function to compute backward transitions (==smoothing)

--- src/predict.jl
+++ src/predict.jl
@@ -1,13 +1,21 @@
 """
 Given an affine transition model
-x_n+1 | x_n ~ N(x_n+1; A x_n, Q)
+x_n+1 | x_n ~ N(x_n+1; A x_n + b, Q)
 and a current state estimate
 x_n ~ N(m, C)
 compute the prediction estimate
-x_n+1 ~ N(A m, A C A^T +Q)
+x_n+1 ~ N(A m + b, A C A^T +Q)
 """
-function predict(m::AbstractVector, C::AbstractMatrix, A::AbstractMatrix, Q::AbstractMatrix)
-    mnew = A * m
+function predict(m::AbstractVector, C::AbstractMatrix, A::AbstractMatrix, b::AbstractVector, Q::AbstractMatrix)
+    mnew = A * m + b
     Cnew = A * C * A' + Q
     return mnew, Cnew
+end
+
+
+function get_backward_transition(m, C, mpred, Cpred, A)
+    G = C * A' / Cpred
+    b = m - G * mpred
+    Λ = C - G * Cpred * G'
+    return G, b, Λ
 end
