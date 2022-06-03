5c4ed8001cff2f62d5602514fc0d485b81bf318b
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2022-06-03 14:39:38 +0300

    Add square-root implementtions for predict and update (#3)
    * Add a square-root prediction function
    
    * Add square-root update
    
    * Make the smoothing test into a testset
    
    * Add a second smoothing test; fix a discovered bug!
    
    * JuliaFormatter
    
    * Fix failing 1.6 tests

--- test/filtsmoothfuncs.jl
+++ test/filtsmoothfuncs.jl
@@ -7,35 +7,67 @@ using LinearAlgebra
     d = 5
 
     m = rand(d)
-    C = rand(d, d) |> Symmetric |> collect
+    CL = rand(d, d)
+    C = CL * CL'
 
     A = rand(d, d)
-    Q = rand(d, d) |> Symmetric |> collect
+    QL = rand(d, d)
+    Q = QL * QL'
     b = zeros(d)
 
-    # Predict
-    mp, Cp = KalmanFilterToolbox.predict(m, C, A, b, Q)
-    @test mp == A * m
-    @test Cp == A * C * A' + Q
+    local mp, Cp
+    @testset "predict" begin
+        mp, Cp = KalmanFilterToolbox.predict(m, C, A, b, Q)
+        @test mp == A * m
+        @test Cp == A * C * A' + Q
+    end
 
-    # Update
+    local mp_sqrt, CpL_sqrt
+    @testset "predict (square-root)" begin
+        mp_sqrt, CpL_sqrt = KalmanFilterToolbox.sqrt_predict(m, CL, A, b, QL)
+        @test mp == mp_sqrt
+        @test Cp ≈ (CpL_sqrt * CpL_sqrt')
+    end
+
     H, b = rand(d, d), rand(d)
     data = rand(d)
-    R = 1e-6I(d)
-    mf, Cf = KalmanFilterToolbox.update(mp, Cp, data, H, b, R)
-    @test norm(H * mf + b - data) < norm(H * mp + b - data)
-    @test norm(Cf) < norm(Cp)
+    R = Matrix(1e-2I, d, d)
+    local mf, Cf
+    @testset "update" begin
+        mf, Cf = KalmanFilterToolbox.update(mp, Cp, data, H, b, R)
+        @test norm(H * mf + b - data) < norm(H * mp + b - data)
+        @test norm(Cf) < norm(Cp)
+    end
 
-    # Update with noiseless zero data
-    H, b = I(d), zeros(d)
-    data = zeros(d)
-    R = zeros(d, d)
-    mf, Cf = KalmanFilterToolbox.update(mp, Cp, data, H, b, R)
-    @test all(abs.(mf) .< 1e-14)
-    @test all(abs.(Cf) .< 1e-14)
+    @testset "update (square-root)" begin
+        RL = sqrt.(R)
+        @test R ≈ RL * RL'
+        mf_sqrt, CfL_sqrt =
+            KalmanFilterToolbox.sqrt_update(mp, CpL_sqrt, data, H, b, sqrt.(R))
+        @test mf ≈ mf_sqrt
+        @test Cf ≈ (CfL_sqrt * CfL_sqrt')
+    end
 
-    # Smooth
-    ms, Cs = KalmanFilterToolbox.smooth(m, C, mf, Cf, A, b, Q)
-    _msp, _Csp = KalmanFilterToolbox.predict(ms, Cs, A, b, Q)
-    @test norm(_msp - data) < norm(mp - data)
+    @testset "update (noiseless zero data)" begin
+        H, b = I(d), zeros(d)
+        data = zeros(d)
+        R = zeros(d, d)
+        mf, Cf = KalmanFilterToolbox.update(mp, Cp, data, H, b, R)
+        @test all(abs.(mf) .< 1e-14)
+        @test all(abs.(Cf) .< 1e-14)
+    end
+
+    local ms, Cs
+    @testset "smooth" begin
+        ms, Cs = KalmanFilterToolbox.smooth(m, C, mf, Cf, A, b, Q)
+        _msp, _Csp = KalmanFilterToolbox.predict(ms, Cs, A, b, Q)
+        @test norm(_msp - data) < norm(mp - data)
+    end
+
+    @testset "smooth (via backward transition)" begin
+        G, b, Λ = KalmanFilterToolbox.get_backward_transition(m, C, mp, Cp, A)
+        ms2, Cs2 = KalmanFilterToolbox.predict(mf, Cf, G, b, Λ)
+        @test ms ≈ ms2
+        @test Cs ≈ Cs2
+    end
 end
