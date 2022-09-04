14896a4fbad8877843621964f4341dd2919c798d
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2022-09-04 15:08:59 +0200

    Add more and better tests

--- test/priors.jl
+++ test/priors.jl
@@ -32,6 +32,29 @@ h = rand()
     @test QH_22_IBM ≈ Qh
 end
 
+@testset "Test non-preconditioned IWP (d=2,q=2)" begin
+    d, q = 2, 2
+
+    iwp = KFT.IWP(d, q)
+    Ah, Qh = KFT.discretize(iwp, h)
+    A, Q = KFT.preconditioned_discretize(iwp)
+    P = KFT.preconditioner(iwp, h)
+    PI = inv(P)
+    @test A ≈ PI * Ah * P
+    @test Q ≈ PI * Qh * PI
+end
+
+@testset "Check that IWP and SDE lead to the same" begin
+    d, q = 1, 2
+
+    iwp = KFT.IWP(d, q)
+    A_iwp, Q_iwp = KFT.discretize(iwp, h)
+    A_sde, Q_sde = KFT.discretize(KFT.to_1d_sde(iwp), h)
+
+    @test A_iwp ≈ A_sde
+    @test Q_iwp ≈ Q_sde
+end
+
 @testset "IOUP with zero drift is IWP" begin
     d, q = 2, 2
 
